e0577d0b1563c019b6e0a56aea8f36102b8ba0ce
lev epshtein <lev_epstein@gmail.com>
2020-11-30 01:22:13 +0200

    commit 1

---
+++ closures/closure.func.py
@@ -0,0 +1,16 @@
+def print_msg(msg):
+    # This is the outer enclosing function
+
+    def printer():
+        # This is the nested function
+        print(msg)
+
+    return printer  # returns the nested function
+
+
+# Now let's try calling this function.
+# Output: Hello
+another = print_msg("Hello")
+another()
+
+
